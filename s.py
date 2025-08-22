1d40fd3c8214559b99a26dfa38106204c41f42e9
erfan_mohammadi <linerfan1378304594@gmail.com>
2025-08-23 03:06:28 +0330

    Update s.py

--- s.py
+++ s.py
@@ -1,8 +1,11 @@
 import pyautogui as pu
 import wikipedia as wk
+import webbrowser
 
-t = str(pu.prompt("search :"))
-
+t = str(pu.prompt("Search:"))
 search = wk.search(t)
 
-pu.alert(search)
+if search:
+    webbrowser.open(f"https://en.wikipedia.org/wiki/{search[0]}")
+else:
+    pu.alert("No results found.")
