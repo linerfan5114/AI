7b26902ccde99e1e1263a1e0860522a0cb9d978d
erfan_mohammadi <linerfan1378304594@gmail.com>
2025-08-23 03:10:30 +0330

    Update s.py

--- s.py
+++ s.py
@@ -1,11 +1,35 @@
 import pyautogui as pu
 import wikipedia as wk
 import webbrowser
+from langdetect import detect
 
-t = str(pu.prompt("Search:"))
-search = wk.search(t)
 
-if search:
-    webbrowser.open(f"https://en.wikipedia.org/wiki/{search[0]}")
+t = str(pu.prompt("search:"))
+
+
+try:
+    language = detect(t)
+
+    if language == 'fa':
+        wk.set_lang("fa")
+
+    elif language == 'en':
+        wk.set_lang("en")
+
+    else:
+        wk.set_lang("en")
+except:
+
+    wk.set_lang("en")
+
+search_results = wk.search(t)
+
+
+if search_results:
+
+    webbrowser.open(wk.page(search_results[0]).url)
+
+
 else:
-    pu.alert("No results found.")
+
+    pu.alert("not.")
